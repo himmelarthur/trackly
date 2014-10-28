09602d936601701f85c2c88b31998e93d5e5cc2d
Arthur Himmel <himmel.arthur@gmail.com>
2014-10-28 15:29:00 +0100

    Added security to API

--- server/api.js
+++ server/api.js
@@ -1,11 +1,12 @@
 var apiRoutes = require('../routes/api');
+var apiAuth = require('./auth/api_auth');
 
 module.exports = function (router) {
 
     router.get('/api/user/:user_id/tracks', apiRoutes.tracks.get);
 
-    router.post('/api/user/:user_id/tracks', apiRoutes.tracks.post);
+    router.post('/api/user/:user_id/tracks', apiAuth, apiRoutes.tracks.post);
 
-    router.delete('/api/user/:user_id/tracks/:track_id', apiRoutes.tracks.delete);
+    router.delete('/api/user/:user_id/tracks/:track_id', apiAuth, apiRoutes.tracks.delete);
 
 };
